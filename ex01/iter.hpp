32fab4b0f615161f9d05d0b502957188d5eb5ad4
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-11-02 12:01:48 +0100

    start on ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 11:56:25 by mdahani           #+#    #+#             */
+/*   Updated: 2025/11/02 12:00:44 by mdahani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+// * Includes
+#include <iostream>
+
+// * Functions
+template<typename T>
+void iter(T &add, const T &len, void (*ptrFunc)(const std::string &str)){
+    
+}
+
+#endif
